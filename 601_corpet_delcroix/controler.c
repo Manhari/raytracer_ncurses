08e785e0a429fb8cd7e59ee8f971e15c5d95a8d8
Kryllarn <Ketreuth@gmail.com>
2018-04-13 09:02:04 +0200

    Update controler.c

--- 601_corpet_delcroix/controler.c
+++ 601_corpet_delcroix/controler.c
@@ -17,30 +17,22 @@ void handler(int code) {
 
 void show_command_synthax(char *program_name) {
     fprintf(stderr,"%s [NOMBRE_D_OBJETS]\n", program_name);
-    fprintf(stderr,"\tWhere :\n\t[NOMBRE_D_OBJETS] is the visualizer IPv4 address\n");
+    fprintf(stderr,"\tWhere :\n\t[NOMBRE_D_OBJETS] is the maximum number of object\n");
 }
 
 /* Programme principal du client */
 int main(int argc, char *argv[]) {
     /* Verification des donnees passees en argument de la commande ./client */
-    if(argc<4) {
+    if(argc<2) {
         fprintf(stderr,"Too few argument to execute %s\n", argv[0]);
         show_command_synthax(argv[0]);
         exit(EXIT_FAILURE);
-    } else if (argc>4) {
+    } else if (argc>2) {
         fprintf(stderr,"Too many argument to execute %s\n", argv[0]);
         show_command_synthax(argv[0]);
         exit(EXIT_FAILURE);
-    } else if (strlen(argv[1])>15) {
+    } else if (atoi(argv[1])==0) {
         fprintf(stderr,"Invalid argument : %s\n", argv[1]);
-        show_command_synthax(argv[0]);
-        exit(EXIT_FAILURE);
-    } else if (atoi(argv[2])<1024) {
-        fprintf(stderr,"Invalid argument : %s\n", argv[2]);
-        show_command_synthax(argv[0]);
-        exit(EXIT_FAILURE);
-    } else if (atoi(argv[3])<1024) {
-        fprintf(stderr,"Invalid argument : %s\n", argv[3]);
         show_command_synthax(argv[0]);
         exit(EXIT_FAILURE);
     }
