ca937708432f7b730cddcb98dd74e3d5ca26eded
Manhari <jeremycorpet@hotmail.fr>
2018-04-13 08:46:20 +0200

    Tab correction

--- src/ncurses.c
+++ src/ncurses.c
@@ -7,34 +7,34 @@
  * Initialization of ncurses.
  */
 void ncurses_initialize() {
-  initscr();
-  cbreak();
-  noecho();
-  keypad(stdscr, TRUE);
-  refresh();
-  curs_set(FALSE);
+	initscr();
+	cbreak();
+	noecho();
+	keypad(stdscr, TRUE);
+	refresh();
+	curs_set(FALSE);
 }
 
 /**
  * Stop ncurses mode.
  */
 void ncurses_stop() {
-  endwin();
+	endwin();
 }
 
 /**
  * Initialization of colors.
  */
 void ncurses_colors() {
-  /* Check colors support */
-  if(has_colors() == FALSE) {
-    ncurses_stop();
-    fprintf(stderr, "No color support for this terminal.\n");
-    exit(EXIT_FAILURE);
-  }
+	/* Check colors support */
+	if(has_colors() == FALSE) {
+		ncurses_stop();
+		fprintf(stderr, "No color support for this terminal.\n");
+		exit(EXIT_FAILURE);
+	}
 
-  /* Activate colors */
-  start_color();
+	/* Activate colors */
+	start_color();
 }
 
 /**
@@ -44,10 +44,10 @@ void ncurses_colors() {
  * @return TRUE if the sizes are OK else returns FALSE
  */
 int ncurses_checksize(int height, int width) {
-  int result = TRUE;
+	int result = TRUE;
 
-  if((COLS < width) || (LINES < height))
-    result = FALSE;
+	if((COLS < width) || (LINES < height))
+	result = FALSE;
 
-  return result;
+	return result;
 }
--- src/scene.h
+++ src/scene.h
@@ -5,49 +5,49 @@
 
 /* A vector */
 typedef struct {
-  double x;           /* x coordinate */
-  double y;           /* y coordinate */
-  double z;           /* z coordinate */
+	double x;           /* x coordinate */
+	double y;           /* y coordinate */
+	double z;           /* z coordinate */
 } vector_t; 
 
 /* An area for the scene */
 typedef struct {
-  double min_x, max_x;
-  double min_y, max_y;
-  double min_z, max_z;
+	double min_x, max_x;
+	double min_y, max_y;
+	double min_z, max_z;
 } area_t;
 
 /* A sphere */
 typedef struct {
-  vector_t center;    /* Center */
-  double radius;      /* Radius */
-  int color;          /* Color */
+	vector_t center;    /* Center */
+	double radius;      /* Radius */
+	int color;          /* Color */
 } sphere_t;
 
 #define MAX_SPHERES 10
 
 /* A scene that gathers spheres */
 typedef struct {
-  int nb;             /* Number of spheres in the scene */
-  area_t area;        /* Area of the scene */
-  int empty[MAX_SPHERES]; /* TRUE if it's empty */
-  sphere_t objs[MAX_SPHERES]; /* Spheres of the scene */
-  vector_t directions[MAX_SPHERES]; /* Directions of the spheres */
-  vector_t camera;    /* Position of the camera */
-  double focal;       /* Focal angle */
+	int nb;             /* Number of spheres in the scene */
+	area_t area;        /* Area of the scene */
+	int empty[MAX_SPHERES]; /* TRUE if it's empty */
+	sphere_t objs[MAX_SPHERES]; /* Spheres of the scene */
+	vector_t directions[MAX_SPHERES]; /* Directions of the spheres */
+	vector_t camera;    /* Position of the camera */
+	double focal;       /* Focal angle */
 } scene_t;
 
 /* A ray */
 typedef struct {
-  vector_t origin;    /* Origin */
-  vector_t direction; /* Direction */
+	vector_t origin;    /* Origin */
+	vector_t direction; /* Direction */
 } ray_t;
 
 /* A picture of pixels */
 typedef struct {
-  int height;         /* Height */
-  int width;          /* Width */
-  int *pixels;        /* Pixels */
+	int height;         /* Height */
+	int width;          /* Width */
+	int *pixels;        /* Pixels */
 } picture_t;
 
 /**
